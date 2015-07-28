93cdb13143094f933dd67d54de69b8b584a7eb33
Marco Wagener <backslashsolutions@gmail.com>
2015-07-28 14:27:43 +0200

    A replica of the AngularJS PhoneCat application.

---
+++ AngularPhoneCat/AngularPhoneCat/appjs/controllers.js
@@ -0,0 +1,10 @@
+﻿var phonecatApp = angular.module("phonecatApp", []);
+
+phonecatApp.controller("PhoneListController", ["$scope", "$http", function ($scope, $http) {
+
+    $http.get("phones/phones.json").success(function (data) {
+        $scope.phones = data;
+    });
+
+    $scope.orderProp = "age";
+}]);
